1c6a14dd8776a1943a88943f98852d55107cdfd7
Hayat6488 <abulhayat619@gmail.com>
2022-10-26 03:59:27 +0600

    navbar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,18 +1,41 @@
-import { Button, Switch } from '@mui/material';
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import React from 'react';
+// import './header.css'
 import { useContext } from 'react';
 import { data } from '../../../Contexts/ContextProvider';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
-    const {mode, setMode} = useContext(data);
+    const { mode, setMode } = useContext(data);
 
     return (
-        <div>
-            <h1>Heading</h1>
-            {
-                mode ? <Button onClick={()=>setMode(!mode)} variant="outlined">Dark</Button> : <Button onClick={()=>setMode(!mode)} variant="outlined">Light</Button>
-            }
+        <div style={{marginBottom: "80px"}}>
+            <AppBar positions="sticky">
+                <Toolbar style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                }}>
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "10px"
+                        }}>
+                        <AutoStoriesIcon style={{ fontSize: "50px" }}></AutoStoriesIcon>
+                        <Typography variant='h4' color="">Admission Aid</Typography>
+                    </div>
+                    <div>
+                        <Button variant="secondary"><Link to='/courses'>Courses</Link></Button>
+                        <Button variant="secondary"><Link to='/FAQs'>FAQs</Link></Button>
+                        <Button variant="secondary"><Link to='/blogs'>Blogs</Link></Button>
+                        {
+                            mode ? <Button onClick={() => setMode(!mode)} variant="secondary">Dark</Button> : <Button onClick={() => setMode(!mode)} variant="error">Light</Button>
+                        }
+                    </div>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 };
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../LayOut/Main";
+import Blogs from "../../Pages/Blogs/Blogs";
+import Courses from "../../Pages/Courses/Courses";
+import Faqs from "../../Pages/Faqs/Faqs";
 
 export const routes = createBrowserRouter([
     {
@@ -7,7 +10,16 @@ export const routes = createBrowserRouter([
         element: <Main></Main>,
         children: [
             {
-                
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/FAQs',
+                element: <Faqs></Faqs>
+            },
+            {
+                path: '/blogs',
+                element: <Blogs></Blogs>
             }
         ]
     }
