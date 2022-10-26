853b55098a4494f656bb3b9bab070a41eca87d8c
Hayat6488 <abulhayat619@gmail.com>
2022-10-26 09:18:16 +0600

    cards added

--- src/Pages/Subject/Subject.js
+++ src/Pages/Subject/Subject.js
@@ -1,6 +1,7 @@
 import './Subject.css'
 import React, { useContext } from 'react';
 import { SubContext } from '../../Contexts/ContextProvider';
+import { Button } from '@mui/material';
 
 
 const Subject = (props) => {
@@ -10,6 +11,12 @@ const Subject = (props) => {
     return (
         <div className={`${mode ? "light" : "dark"}`}>
             <img src={img} alt="" />
+            <p>Instructor: {instructor.name}</p>
+            <div className='sub-details'>
+                <p>Class: {lec}</p>
+                <p>Tk. {price}</p>
+                <Button variant="">Details</Button>
+            </div>
         </div>
     );
 };
